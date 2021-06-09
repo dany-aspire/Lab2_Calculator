e7ae741ab0752380f40e395e75d3224cb3ecd8b1
Dan <dannieabraham@gmail.com>
2021-06-09 20:28:49 +0530

    Adding division unit tests

---
+++ CalcTest/UnitTestDivision2.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Lab2_Calculator;
+
+namespace CalcTest
+{
+    [TestFixture]
+    public class UnitTestDivision2
+    {
+
+        Calc calc = new Calc();
+
+        [Test]
+        public void Division_of_two_integers2()
+        {
+            double a = -6, b = 2;//arrange
+            calc.SetFirstNumber(a);//arrange
+            calc.SetSecondNumber(b);//arrange
+            double res = calc.GetDivision(); //act
+            Assert.AreEqual(res, -3); //assert
+        }
+
+
+
+    }
+}
+
---
+++ CalcTest/UnitTestDivision3.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Lab2_Calculator;
+
+namespace CalcTest
+{
+    [TestFixture]
+    public class UnitTestDivision3
+    {
+
+        Calc calc = new Calc();
+
+        [Test]
+        public void Division_of_two_integers3()
+        {
+            double a = -6, b = -2;//arrange
+            calc.SetFirstNumber(a);//arrange
+            calc.SetSecondNumber(b);//arrange
+            double res = calc.GetDivision(); //act
+            Assert.AreEqual(res, 3); //assert
+        }
+
+
+
+    }
+}
+
