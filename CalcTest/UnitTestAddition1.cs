8924464d94114bdb1084db64d3353c93e4df9e47
Dan <dannieabraham@gmail.com>
2021-06-09 20:01:16 +0530

    Adding addition unit tests

---
+++ CalcTest/UnitTestAddition1.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Lab2_Calculator;
+
+namespace CalcTest
+{
+    [TestFixture]
+    public class UnitTestAddition1
+    {
+
+        Calc calc = new Calc();
+
+        [Test]
+        public void Addition_of_two_integers1()
+        {
+            double a = 5, b = 4;//arrange
+            calc.SetFirstNumber(a);//arrange
+            calc.SetSecondNumber(b);//arrange
+            double res = calc.GetAddition(); //act
+            Assert.AreEqual(res, 9); //assert
+        }
+
+    }
+}
+
---
+++ CalcTest/UnitTestAddition3.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Lab2_Calculator;
+
+namespace CalcTest
+{
+    [TestFixture]
+    public class UnitTestAddition3
+    {
+
+        Calc calc = new Calc();
+
+        [Test]
+        public void Addition_of_two_integers3()
+        {
+            double a = -5, b = -4;//arrange
+            calc.SetFirstNumber(a);//arrange
+            calc.SetSecondNumber(b);//arrange
+            double res = calc.GetAddition(); //act
+            Assert.AreEqual(res, -9); //assert
+        }
+
+
+
+    }
+}
+
