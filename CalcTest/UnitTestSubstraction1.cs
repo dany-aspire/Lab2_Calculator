b9d81651d35b3007b2bc2790872e52f5c97d89ff
Dan <dannieabraham@gmail.com>
2021-06-09 20:10:21 +0530

    Adding substraction unit tests

---
+++ CalcTest/UnitTestSubstraction1.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Lab2_Calculator;
+
+namespace CalcTest
+{
+    [TestFixture]
+    public class UnitTestSubstraction1
+    {
+
+        Calc calc = new Calc();
+
+        [Test]
+        public void Substraction_of_two_integers1()
+        {
+            double a = 5, b = 4;//arrange
+            calc.SetFirstNumber(a);//arrange
+            calc.SetSecondNumber(b);//arrange
+            double res = calc.GetSubtraction(); //act
+            Assert.AreEqual(res, 1); //assert
+        }
+
+
+
+    }
+}
+
---
+++ CalcTest/UnitTestSubstraction3.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Lab2_Calculator;
+
+namespace CalcTest
+{
+    [TestFixture]
+    public class UnitTestSubstraction3
+    {
+
+        Calc calc = new Calc();
+
+        [Test]
+        public void Substraction_of_two_integers3()
+        {
+            double a = 5, b = 4;//arrange
+            calc.SetFirstNumber(a);//arrange
+            calc.SetSecondNumber(b);//arrange
+            double res = calc.GetSubtraction(); //act
+            Assert.AreEqual(res, 1); //assert
+        }
+
+
+
+    }
+}
+
